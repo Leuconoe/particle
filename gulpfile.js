7d333ae116a7ca619ec5b79376a71135451d74b3
김태련 <n.leuconoe@skonec.com>
2021-08-20 14:07:53 +0900

    fix sweet-scroll

--- gulpfile.js
+++ gulpfile.js
@@ -85,7 +85,6 @@ gulp.task('watch', function() {
   gulp.watch('src/js/**/*.js', gulp.series(['js', 'jekyll-rebuild']));
   gulp.watch('src/fonts/**/*.{tff,woff,woff2}', gulp.series(['fonts']));
   gulp.watch('src/img/**/*.{jpg,png,gif}', gulp.series(['imagemin']));
-  //gulp.watch('src/skill/**/*.*', gulp.series(['skill']));
   gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], gulp.series(['jekyll-rebuild']));
 });
 
